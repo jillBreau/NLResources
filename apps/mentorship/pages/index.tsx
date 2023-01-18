924b219d8dc3465a1ab0319a5a0d7085addc7221
Jillian Breau <jmbreau@mun.ca>
2023-01-18 13:13:55 -0330

    implement Prisma with Postgresql in mentorship app

--- apps/mentorship/pages/index.tsx
+++ apps/mentorship/pages/index.tsx
@@ -1,12 +1,37 @@
-export function Index() {
+import { GetStaticProps } from 'next/types';
+import prisma from '../lib/prisma';
+import { Type, Resource } from '@prisma/client'
+
+export interface ResourcesProp {
+  resources: Resource[]
+}
+
+export const getStaticProps: GetStaticProps<ResourcesProp> = async () => {
+  const resources: Resource[] = await prisma.resource.findMany({
+    where: { reviewed: true, type: Type.MENTORSHIP },
+    include: {
+      organization: {
+        select: { name: true },
+      },
+    },
+  });
+  return {
+    props: { resources },
+    revalidate: 10,
+  };
+};
+
+export function Index(props: ResourcesProp) {
   return (
     <div>
       <h1 className="bg-blue-500 p-2 font-mono">
         Mentorship main page 👋
       </h1>
-      <p className="text-big text-green-500">
-        hi
-      </p>
+      <ul className="text-green-500">
+        {props.resources.map((resource) => {
+          return <li key={resource.id} onClick={() => console.log(resource)}>{resource.title}</li>
+        })}
+      </ul>
     </div>
   )
 }
